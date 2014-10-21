716ae82d4ba77b10794a96863fe86a2ad88e3066
sylvain-galineau <galineau@adobe.com>
2014-10-21 12:29:48 -0700

    Use shorter copyright/license header

--- Gruntfile.js
+++ Gruntfile.js
@@ -15,19 +15,9 @@ limitations under the License.
 
 module.exports = function(grunt) {
     
-    var copyright = '/*!\n' +
-                    'Copyright 2014 Adobe Systems Inc.;\n' +
-                    'Licensed under the Apache License, Version 2.0 (the "License");\n' +
-                    'you may not use this file except in compliance with the License.\n' +
-                    'You may obtain a copy of the License at\n' +
-                    '\n' +
-                    'http://www.apache.org/licenses/LICENSE-2.0\n' +
-                    '\n' +
-                    'Unless required by applicable law or agreed to in writing, software\n' +
-                    'distributed under the License is distributed on an "AS IS" BASIS,\n' +
-                    'WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.\n' +
-                    'See the License for the specific language governing permissions and\n' +
-                    'limitations under the License.\n' +
+    var copyright = '/*\n' +
+                    'Copyright 2014 Adobe Systems Incorporated. Licensed under the Apache 2.0 License.\n' +
+                    'http://www.apache.org/licenses/LICENSE-2.0.html\n' +
                     '*/\n\n';
 
 
@@ -58,7 +48,8 @@ module.exports = function(grunt) {
 
         uglify: {
             options: {
-                preserveComments: 'some'
+                preserveComments: 'false',
+                banner: '<%= copyright %>'
             },
             dist: {
                 src: ['<%= concat.dist.dest %>'],
